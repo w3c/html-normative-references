457d43f19328c41b50a536b3875c0bb3a8f49518
Robin Berjon <robin@berjon.com>
2014-07-18 14:32:16 +0200

    added sorting

--- normative-references-rock-my-pants.js
+++ normative-references-rock-my-pants.js
@@ -1,7 +1,8 @@
 /* jshint -W100 */
 
+var $tbody = $("#data tbody");
+
 $.getJSON("data.json", function (data) {
-    var $tbody = $("#data tbody");
     $.each(data, function (ref, info) {
         var $tr = $("<tr><th></th></tr>")
                     .addClass(info.type)
@@ -28,6 +29,24 @@ $.getJSON("data.json", function (data) {
         $td.clone().text(info.stable ? "✓" : "☠").addClass("stability").appendTo($tr);
         $td.clone().html(info.notes).appendTo($tr);
         $tbody.append($tr);
+    });
+    
+    // sorting
+    $("body").on("click", ".sortable", function (ev) {
+        var $th = $(ev.target)
+        ,   column = $th.prevAll("th").length
+        ,   colVal = function (el) { return $(el).find("td, th")[column].textContent; }
+        ,   rows = $tbody.find("tr")
+                            .clone()
+                            .sort(function (a, b) {
+                                if (colVal(a) < colVal(b)) return -1;
+                                if (colVal(a) > colVal(b)) return 1;
+                                return 0;
+                            })
+        ;
+        $tbody.empty().append(rows);
+        $(".sorting").removeClass("sorting").addClass("sortable");
+        $th.removeClass("sortable").addClass("sorting");
     });
 });
 
